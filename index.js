38b8ee1b914eef63fd67a489420ccbdfdf37d400
sahil-dx <sahilbarwal.dx@gmail.com>
2025-12-29 18:10:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Bawa Good Morning From Express JS')
+    res.send(' Good Morning From Express JS')
 })
 
 app.listen(5000, () => {
