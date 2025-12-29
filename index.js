60348e88bd265d3ac699e53cd159947babc50aa5
sahil-dx <sahilbarwal.dx@gmail.com>
2025-12-29 11:31:09 +0530

    basic pipeline

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('', (req, res) => {
+    res.send('Good Morning From Express JS')
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port no 5000");
+});
